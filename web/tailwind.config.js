ee13329854975c84ac24aeee2cc4913bdbd7c1f5
PauloSoares <paulosjunior7@gmail.com>
2025-07-30 19:47:38 -0300

    feat: adicionar configurações de breakpoints no Tailwind CSS

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,6 +6,14 @@ export default {
       fontFamily: {
         sans: ["Outfit", "ui-sans-serif", "system-ui"],
       },
+      screens: {
+        'xs': '475px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
     },
   },
   plugins: [],
